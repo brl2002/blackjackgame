e2fa68f49d947bf70264f4e1fc2e7e22170014f9
Minsoo Lee <06leemin@gmail.com>
2020-08-14 15:56:11 -0400

    Adding Game and Seat classes.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Game : MonoBehaviour {
+
+	[SerializeField]
+	private Seat m_SeatPrefab;
+
+	[SerializeField]
+	private Transform[] m_Positions;
+
+	private List<Seat> m_Seats = new List<Seat>();
+
+	private void Awake() {
+		m_Seats.Add(Instantiate(m_SeatPrefab));
+	}
+
+}
---
+++ Assets/Scripts/Seat.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Seat : MonoBehaviour {
+
+	public enum Type {
+		PLAYER,
+		DEALER
+	}
+
+	private Vector2 m_Position;
+
+	public void SetPosition(Vector2 pos) {
+		m_Position = pos;
+	}
+
+}
